3f46ec0ac228af3b140d2114cf03b9e3c272442d
Brian Scherner <brianscherner@gmail.com>
2024-01-11 14:23:11 -0800

    Create Park model and create database.

--- ParksLookupApi/Program.cs
+++ ParksLookupApi/Program.cs
@@ -1,6 +1,19 @@
+using ParksLookupApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ParksLookupApiContext>(
+  dbContextOptions => dbContextOptions
+    .UseMySql(
+      builder.Configuration["ConnectionStrings:DefaultConnection"],
+      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+    )
+  )
+);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
