5d0998d8aceff153cfd78b8b1c2ed0bc7eafae13
Brian Scherner <brianscherner@gmail.com>
2024-01-11 15:11:15 -0800

    Add functionality to Create a Park.

--- ParksLookupApi/Controllers/ParksController.cs
+++ ParksLookupApi/Controllers/ParksController.cs
@@ -33,5 +33,13 @@ namespace ParksLookupApi.Controllers
 
       return park;
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
   }
 }
